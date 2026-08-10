89be41d567548623539f8605c535ff3d77c27bc1
gumlaybeg <gumlaybeg15@protonmail.com>
2026-08-11 01:28:14 +0800

    Add media3.dash dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,7 @@ dependencies {
     implementation(libs.media3.session)
     implementation(libs.media3.okhttp)
     implementation(libs.media3.ui)
+    implementation(libs.media3.dash)
     implementation(libs.squigglyslider)
 
     implementation(libs.room.runtime)
